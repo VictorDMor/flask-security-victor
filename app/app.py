d6c2fd67486321e331a475b668aeb47ce92922ca
Victor Moraes <victordmor@Arsenal.local>
2016-03-22 15:31:27 -0300

    Creating run.py

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, url_for, redirect, render_template, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required, current_user
+from flask_security.utils import encrypt_password
+import flask_admin
+from flask_admin.contrib import sqla
+from flask_admin import helpers as admin_helpers
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
